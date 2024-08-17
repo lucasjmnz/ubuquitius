51e6c3a81a93121a403c753c00b198c5054768f9
lucasjmnz <lucascrfortnite@gmail.com>
2024-08-16 19:44:55 -0600

    Create biblioteca.py

---
+++ biblioteca.py
@@ -0,0 +1,10 @@
+meme = {
+    "XD : es un digamos una forma de expresar alegria "
+    "funar : es una palabra digamos como de odio o insultos"
+    "cringe : es digamos cuando dices uy q raro "
+    "LOL : a ver digamos una respuesta a algo gracioso es como XD"
+    "GHOSTEAR : cuando revisas o buscas mucho el perfil de una persona y estas siempre al tanto"
+    }
+    print meme([texto])
+    else :
+        print("no se encontro su meme")
